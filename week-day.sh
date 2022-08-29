1189f0186c0ee63002134113ba034d006c9f1d94
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-08-29 13:06:35 +0530

    Added: week-day logic

---
+++ week-day.sh
@@ -0,0 +1,25 @@
+read -p "enter week-num:" num
+if((num=="1"))
+then
+    echo "sunday"
+elif((num=="2"))
+then
+    echo "Monday"
+elif((num=="3"))
+then
+    echo "Tuesday"
+elif((num=="4"))
+then
+    echo "Wednesday"
+elif((num=="5"))
+then
+    echo "Thursday"
+elif((num=="6"))
+then
+    echo "Friday"
+elif((num=="7"))
+then
+    echo "Saturday"
+else
+    echo "Invalid week number"
+fi
